dafe3da37701c79643c3807e5727e589a3fa703f
C-C0des <34481244+C-C0des@users.noreply.github.com>
2018-10-18 13:54:59 -0400

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+
+-- FOOD DATABASE SCHEMA --
+DROP DATABASE IF EXISTS foodDB
+
+CREATE DATABASE foodDB;
+
+USE foodDB;
+
+CREATE TABLE Recipe
+(
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	recipe_name varchar(30),
+	instructions varchar(800) NOT NULL,
+    cuisine varchar(30) NOT NULL
+);
+
+CREATE TABLE Ingredient
+(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ingredient_name varchar(50),
+	category varchar(50)
+);
+
+-- Join tables code later ---
+-- When a user searches up an ingredient, join tables (recipes and ingredients) will match the ingredients 
+--  to the recipes in the recipe table matching that ingredient 
+
+
+-- For later, maybe create another database that has users information such as username, etc --
+-- and each user can access recipes on their own page --
+
+
