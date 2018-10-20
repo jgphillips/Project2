949f3f9291a184b9c5ccaf675dec3cbc27b3e431
Jam Test <jameelphillips_5@hotmail.com>
2018-10-20 15:16:12 -0400

    changes to the recipe search

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,4 +21,19 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+  app.get("/api/ingredient/:ingredient", function(req,res){
+    db.recipe.findAll({ where: { ingredient: req.params.ingredient } }).then(function(dbRecipes){
+      res.json(dbRecipes);
+    });
+  });
+  
+  app.get("/api/search/:name", function(req,res){
+    db.recipe.findAll({ where: { name: req.params.name } }).then(function(dbRecipes){
+      res.json(dbRecipes);
+    });
+  });
+
 };
+
+  
