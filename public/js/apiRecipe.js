12a6b7125d98cf9ba0ec168a0a8ff2df450ff67c
Jam Test <jameelphillips_5@hotmail.com>
2018-10-23 20:28:53 -0400

    Recipe search API

--- public/js/apiRecipe.js
+++ public/js/apiRecipe.js
@@ -1,4 +1,4 @@
-var API = {
+/*var API = {
     getRecipe: function() {
       return $.ajax({
         url: "api/search",
@@ -6,3 +6,23 @@ var API = {
       });
     }
   };
+*/
+$(document).ready(function(){
+  $(document).on("click", "#btnSearchRecipe", function(event){
+    event.preventDefault();
+    var searchParameter = $("#searchRecipe").val().trim();
+    console.log(searchParameter);
+
+    var queryURL = "https://api.edamam.com/search?q=" + searchParameter + "&app_id=d8a01ad1&app_key=ec212c380911dfbd6ccc557d4ce89405&from=0&to=10&calories=591-722&health=alcohol-free";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response){
+      console.log(response);
+    });
+  });
+});
+
+
+
